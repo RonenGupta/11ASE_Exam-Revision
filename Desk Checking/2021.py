66e8f6c1feec0c5d4ce094747b3c89fb94f6ec9e
RonenGupta <160193990+RonenGupta@users.noreply.github.com>
2025-08-28 15:09:48 +1000

    Update sigma.py

---
+++ Desk Checking/2021.py
@@ -0,0 +1,19 @@
+list = [33, 38, 40, 44, 50, 60, 75]
+low = 1
+high = 7
+found = False
+SearchItem = int(input("SearchItem: "))
+while found == False and high > low:
+    index = low + high
+    index = int(index / 2)
+    if SearchItem == list[index]:
+        found = True
+    else:
+        if SearchItem < list[index]:
+            high = round(index) #
+        else:
+            low = round(index) #
+if found == True:
+    print("Found at ", index + 1)
+else:
+    print("Not found")
---
+++ sumpython.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+baaaaaa = np.array([[2, 7, 6],
+                      [9, 5, 1],
+                      [4, 3, 8]])
+
+def squarespace(baaaaaa):
+    for i in range(3):
+        if baaaaaa[i][0] + baaaaaa[i][1] + baaaaaa[i][2] == 15:
+            if baaaaaa[0][i] + baaaaaa[1][i] + baaaaaa[2][i] == 15:
+                print("afbauIFBAFUEIABFAEVBNUILV")
+                
+squarespace(baaaaaa)
+
